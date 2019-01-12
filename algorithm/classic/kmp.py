50412b06e3099233e3fc9d982be0b291fa86805f
盛广智 <sheng@shengguangzhideMacBook-Pro.local>
2019-01-12 18:28:11 +0800

    implement kmp and solution of quiz.

---
+++ algorithm/classic/kmp.py
@@ -0,0 +1,17 @@
+# -*-encoding:utf-8-*-
+def build_next(p):
+    """
+    """
+    return []
+
+
+def kmp_index(t, p):
+    """
+    """
+    return 0
+
+
+if __name__ == '__main__':
+    target = ''
+    pattern = ''
+    print 'Index of', pattern, 'in', target, 'is ', kmp_index(target, pattern), '.'
