d3beadd7e97e138d67924824ee87219acc86145a
Sheng Guangzhi <vincentsheng@126.com>
2019-04-05 13:12:40 +0800

    1. Update algorithm code 2. Simplify directory structure.

--- algorithm/python_version/basic/kmp.py
+++
@@ -1,9 +0,0 @@
-def build_next():
-    return []
-
-def kmp_index():
-    return None
-
-if __name__ == '__main__':
-    target = ''
-    pattern = ''
--- algorithm/python_version/question/han_nuo_ta.py
+++
@@ -1,6 +0,0 @@
-def calc_step(n):
-    if n == 1:
-        return 1
-    return 2 * calc_step(n-1) + 1
-
-print calc_step(500)
